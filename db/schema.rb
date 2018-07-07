0fd69533c6eec567c543bd5d341c7497fd2b054b
lehall2 <lehall2@ncsu.edu>
2018-07-07 08:35:04 -0400

    Added Welcome Controller Styling and Index, and Show Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701121711) do
+ActiveRecord::Schema.define(version: 20180704133129) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
