a92941b161f33e2a3f3222a78d805a44406ea04f
lehall2 <lehall2@ncsu.edu>
2018-07-08 20:04:52 -0400

    Added Redcarpet and Pygments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
+    class HTMLwithPygments < Redcarpet::Render::HTML
+        def block_code(code, language)
+            Pygments.highlight(code, lexer: language)
+        end
     def markdown(content)
-        renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+        renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
         options = {
             autolink: true,
             no_intra_emphasis: true,
