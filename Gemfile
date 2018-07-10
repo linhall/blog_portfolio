124eb41434b44abe7e35bbc6a3d926008cc30ba7
lehall2 <lehall2@ncsu.edu>
2018-07-10 05:43:44 -0400

    Added will_paginate gem

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'pygments.rb', '~> 1.2.1'
 gem 'redcarpet', '~>3.4.0'
 gem 'devise', '~> 4.4', '>= 4.4.3'
 gem 'friendly_id', '~> 5.2', '>= 5.2.4'
+gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @posts = Post.all.order("created_at desc")
+        @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page:2)
     end
 
     def new
