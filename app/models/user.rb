48161f84796e2f08484ae1cf93cb9fdbe8cf890c
lehall2 <lehall2@ncsu.edu>
2018-07-21 21:51:31 -0400

    Removed registration ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # 
+  # Add :registerable if new users are needed.
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :registerable
+         :recoverable, :rememberable, :trackable, :validatable
 extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
